a1a6a029edacd301084b41098ca4244ff89bcc9f
Max <maxwil2001@hotmail.com>
2020-02-14 15:57:10 +0100

    DoTween

---
+++ Unity Project/Gegeven Game/Assets/Scripts/Max/Damagable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damagable : MonoBehaviour
+{
+    public int Health;
+
+    public int enemyHealth; 
+    public float enemyKnockedOutTime = 3f;
+
+    public void PlayerDamage(int damage)
+    {
+        Health -= damage;
+
+    }
+
+    public void EnemyDamage(int damage)
+    {
+        Health -= damage;
+
+    }
+
+    public IEnumerator KnockOutTime()
+    {
+        yield return new WaitForSeconds(enemyKnockedOutTime);
+    }
+
+}
