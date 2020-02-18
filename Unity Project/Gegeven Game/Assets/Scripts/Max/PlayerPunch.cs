da8e27cc127999fa38ba577137b32389117f2282
Max <maxwil2001@hotmail.com>
2020-02-18 11:03:40 +0100

    punch mechanic

--- Unity Project/Gegeven Game/Assets/Scripts/Max/PlayerPunch.cs
+++ Unity Project/Gegeven Game/Assets/Scripts/Max/PlayerPunch.cs
@@ -1,17 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XboxCtrlrInput;
+
+public enum PunchStatus
+{
+    IdlePunch,
+    punching
+}
 
 public class PlayerPunch : MonoBehaviour
 {
+    public PunchStatus PunchStatus;
 
-    void Start()
+    public Animator animator;
+
+    private BoxCollider2D boxCollider;
+
+    private void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
-        
+        SetValues();
+
+        Punch();
+    }
+
+    private void SetValues()
+    {
+        animator.SetInteger("State", (int)PunchStatus);
+    }
+
+    private void Punch()
+    {
+
+        if (XCI.GetButton(XboxButton.X))
+        {
+            PunchStatus = PunchStatus.punching;
+            boxCollider.enabled = true;
+        }
+        else
+        {
+            PunchStatus = PunchStatus.IdlePunch;
+            StartCoroutine(PunchDelay());
+            boxCollider.enabled = false;
+        }
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    PunchStatus = PunchStatus.punching;
+        //    boxCollider.enabled = true;
+        //}
+        //else
+        //{
+        //////PunchStatus = PunchStatus.IdlePunch;
+        //////StartCoroutine(PunchDelay());
+        //////boxCollider.enabled = false;
+        //}
+    }
+
+    private IEnumerator PunchDelay()
+    {
+        yield return new WaitForSeconds(1.5f);
     }
 }
