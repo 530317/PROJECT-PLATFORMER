7b0835052cc43747cc87d66c52e26def7cb7c9ff
kelvin de Jong <adelaar141@gmail.com>
2020-02-21 14:45:24 +0100

    player update

--- Unity Project/Gegeven Game/Assets/Scripts/kelvin/PlayerControler2.cs
+++ Unity Project/Gegeven Game/Assets/Scripts/kelvin/PlayerControler2.cs
@@ -11,29 +11,40 @@ public class PlayerControler2 : MonoBehaviour
     private float xAxis;
 
     private Rigidbody2D rigiBody;
+    private SpriteRenderer spriteRen;
 
     public int jumpsvalue;
     private int jumps;
 
     private void Start()
     {
+        spriteRen = GetComponent<SpriteRenderer>();
         rigiBody = GetComponent<Rigidbody2D>();
         jumps = jumpsvalue;
+        spriteRen.flipX = false;
     }
 
     private void FixedUpdate()
     {
         xAxis = XCI.GetAxis(XboxAxis.LeftStickX);
         rigiBody.velocity = new Vector2(xAxis * moveSpeed, rigiBody.velocity.y);
+       
     }
     private void Update()
     {
-        if (XCI.GetButtonDown(XboxButton.A)&& jumps > 0)
+        if (XCI.GetButtonDown(XboxButton.A) && jumps > 0)
         {
             rigiBody.velocity = Vector2.up * force;
             jumps--;
         }
-        
+        if (xAxis > 0.1)
+        {
+            spriteRen.flipX = false;
+        }
+        if (xAxis < 0.1)
+        {
+            spriteRen.flipX = true;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
