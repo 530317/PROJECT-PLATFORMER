e7b17ae1bf50be8acbc71689e1db4a70a7f0f444
Max <maxwil2001@hotmail.com>
2020-03-13 13:28:57 +0100

    portal update

--- Unity Project/Gegeven Game/Assets/Scripts/Max/Player/PickupShipPart.cs
+++ Unity Project/Gegeven Game/Assets/Scripts/Max/Player/PickupShipPart.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PickupShipPart : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]private int amountOfPartsPickedUp;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("shipPart"))
+        {
+            amountOfPartsPickedUp++;
+            Destroy(collision.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        RepairShip();
+    }
+
+    private void RepairShip()
+    {
+        if(amountOfPartsPickedUp >= 4)
+        {
+            //do a thing
+        }
     }
 }
