141df2852369b390546314758e36ee769ed53714
Max <maxwil2001@hotmail.com>
2020-03-31 11:51:38 +0200

    grunt en animatie dingen

--- Unity Project/Gegeven Game/Assets/Scripts/Max/Player/PlayerPunch.cs
+++ Unity Project/Gegeven Game/Assets/Scripts/Max/Player/PlayerPunch.cs
@@ -3,35 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using XboxCtrlrInput;
 
-public enum PunchStatus
-{
-    IdlePunch,
-    punching
-}
-
 public class PlayerPunch : MonoBehaviour
 {
-    public PunchStatus PunchStatus;
 
-    public Animator animator;
+    PlayerDirection playerDirection;
 
-    private BoxCollider2D boxCollider;
-
-    private void Start()
-    {
-        boxCollider = GetComponent<BoxCollider2D>();
-    }
+    [SerializeField] CircleCollider2D rightCollider;
+    [SerializeField] CircleCollider2D leftCollider;
 
     void Update()
     {
-        SetValues();
-
         Punch();
-    }
-
-    private void SetValues()
-    {
-        animator.SetInteger("State", (int)PunchStatus);
     }
 
     private void Punch()
@@ -39,31 +21,39 @@ public class PlayerPunch : MonoBehaviour
 
         if (XCI.GetButton(XboxButton.X))
         {
-            PunchStatus = PunchStatus.punching;
-            boxCollider.enabled = true;
+            playerDirection = PlayerDirection.punch;
         }
         else
         {
-            PunchStatus = PunchStatus.IdlePunch;
             StartCoroutine(PunchDelay());
-            boxCollider.enabled = false;
         }
 
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    PunchStatus = PunchStatus.punching;
-        //    boxCollider.enabled = true;
-        //}
-        //else
-        //{
-        //////PunchStatus = PunchStatus.IdlePunch;
-        //////StartCoroutine(PunchDelay());
-        //////boxCollider.enabled = false;
-        //}
+    
     }
+
+    private void CheckDirectionAndPunch()
+    {
+        if(XCI.GetButton(XboxButton.X) && gameObject.GetComponent<SpriteRenderer>().flipX == true)
+        {
+            playerDirection = PlayerDirection.punch;
+        }
+    }
+
+    public void ActivateRightCollider()
+    {
+        rightCollider.enabled = true;
+        StartCoroutine(PunchDelay());
+        rightCollider.enabled = false;
+    }
+
+    public void ActivateLeftCollider()
+    {
+        leftCollider.enabled = true;
+    }
+
 
     private IEnumerator PunchDelay()
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1f);
     }
 }
